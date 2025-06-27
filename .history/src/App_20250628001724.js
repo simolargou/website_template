515a9971e5dcdec44bc11c425fe1ed08edb47c21
simo largou <82075769+simolargou@users.noreply.github.com>
2025-06-28 00:22:52 +0200

    Initial commit: React-App erstellt und Git initialisiert

--- .history/src/App_20250628001724.js
+++ .history/src/App_20250628001724.js
@@ -6,14 +6,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         
-       
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Start the project
+          Learn React
         </a>
       </header>
     </div>
